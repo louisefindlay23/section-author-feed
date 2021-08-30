494b0817e77456bd130a9838cf8e3a0cd65531b2
Louise Findlay <louise@louisefindlay.com>
2021-08-30 17:23:38 +0100

    Added XML parser

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const axios = require("axios");
 const url = "https://www.section.io/engineering-education/index.xml";
+const xml2js = require("xml2js");
+const parser = new xml2js.Parser({ attrkey: "ATTR" });
 
 axios
     .get(url)
-    .then(function (response) {
-        response = response.text();
-        console.info(response);
-    })
+    .then((res) => res.data)
     .catch(function (err) {
         console.error(err);
     })
-    .then(function () {
-        console.log("Works");
+    .then(function (res) {
+        res = new window.DOMParser().parseFromString(res, "text/xml");
+        console.log(res);
+        const titles = res.querySelectorAll("title").forEach((title) => {
+            console.log(title);
+        });
     });
