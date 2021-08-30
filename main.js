fe82e3d9a80609772adc77b3febd3b743d68babd
Louise Findlay <louise@louisefindlay.com>
2021-08-30 18:15:49 +0100

    Renders JSON feed to /feed/all

--- main.js
+++ main.js
@@ -17,34 +17,37 @@ console.info("Listening on " + port);
 
 // Pages
 app.get("/", function (req, res) {
-    res.render("pages/index");
+    res.render("../pages/index");
+});
+
+app.get("/feed/all", function (req, res) {
+    // Get Section RSS Feed
+    axios
+        .get(url)
+        .then((res) => res.data)
+        .catch(function (err) {
+            console.error(err);
+        })
+        .then(function (result) {
+            // Parse XML
+            parser.parseString(result, function (err, result) {
+                if (!err) {
+                    // Convert to JSON
+                    let feed = JSON.stringify(result.rss.channel);
+                    feed = JSON.parse(feed);
+                    let posts = feed[0].item;
+                    res.json(posts);
+                    //console.info(posts);
+                    posts = JSON.stringify(posts);
+                    fs.writeFile("data/all.json", posts, function (err) {
+                        if (err) return console.error(err);
+                        console.info("Saved Section EngEd Feed");
+                    });
+                } else {
+                    console.error(err);
+                }
+            });
+        });
 });
 
 // Function getFeed
-
-// Get Section RSS Feed
-axios
-    .get(url)
-    .then((res) => res.data)
-    .catch(function (err) {
-        console.error(err);
-    })
-    .then(function (res) {
-        // Parse XML
-        parser.parseString(res, function (err, result) {
-            if (!err) {
-                // Convert to JSON
-                let feed = JSON.stringify(result.rss.channel);
-                feed = JSON.parse(feed);
-                let posts = feed[0].item;
-                //console.info(posts);
-                posts = JSON.stringify(posts);
-                fs.writeFile("data/all.json", posts, function (err) {
-                    if (err) return console.error(err);
-                    console.info("Saved Section EngEd Feed");
-                });
-            } else {
-                console.error(err);
-            }
-        });
-    });
