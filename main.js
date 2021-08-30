1bd5deccd1771a68b523f16b08621f8b67fba84a
Louise Findlay <louise@louisefindlay.com>
2021-08-30 18:05:55 +0100

    Saved feed to JSON file

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 // Node Modules
-
 const axios = require("axios");
 const url = "https://www.section.io/engineering-education/index.xml";
 const xml2js = require("xml2js");
 const parser = new xml2js.Parser({ attrkey: "ATTR" });
+const fs = require("fs");
+const express = require("express");
+const ejs = require("ejs");
+const app = express();
+const port = 3000;
+
+// Initialise Express
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.listen(port);
+console.info("Listening on " + port);
+
+// Pages
+app.get("/", function (req, res) {
+    res.render("pages/index");
+});
+
+// Function getFeed
 
 // Get Section RSS Feed
 axios
@@ -19,8 +36,13 @@ axios
                 // Convert to JSON
                 let feed = JSON.stringify(result.rss.channel);
                 feed = JSON.parse(feed);
-                const posts = feed[0].item;
-                console.info(posts);
+                let posts = feed[0].item;
+                //console.info(posts);
+                posts = JSON.stringify(posts);
+                fs.writeFile("data/all.json", posts, function (err) {
+                    if (err) return console.error(err);
+                    console.info("Saved Section EngEd Feed");
+                });
             } else {
                 console.error(err);
             }
